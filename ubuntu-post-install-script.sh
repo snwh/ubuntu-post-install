ab550900290dcfc807e3bceb5d3f1390093f0634
Sam Hewitt <hewittsamuel@gmail.com>
2014-06-14 19:41:21 -0400

    Modularization & cleanup.

---
+++ ubuntu-post-install-script.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# -*- Mode: sh; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Authors:
+#   Sam Hewitt <hewittsamuel@gmail.com>
+#
+# Description:
+#   A post-installation bash script for Ubuntu
+#
+# Legal Stuff:
+#
+# This script is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 3.
+#
+# This script is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <https://www.gnu.org/licenses/gpl-3.0.txt>
+
+echo ''
+echo '#----------------------------------#'
+echo '#     Ubuntu Post-Install Script   #'
+echo '#----------------------------------#'
+
+#----- FUNCTIONS -----#
+
+. ./functions/cleanup
+. ./functions/codecs
+. ./functions/config
+. ./functions/customize
+. ./functions/design
+. ./functions/development
+. ./functions/favourites
+. ./functions/gnome
+. ./functions/system
+. ./functions/sysupgrade
+. ./functions/themes
+. ./functions/thirdparty
+
+#----- MAIN FUNCTIONS -----#
+
+# Quit
+function quit {
+read -p "Are you sure you want to quit? (Y)es, (N)o " REPLY
+case $REPLY in
+    [Yy]* ) exit 99;;
+    [Nn]* ) clear && main;;
+    * ) clear && echo 'Sorry, try again.' && quit;;
+esac
+}
+
+# Main
+function main {
+echo ''
+echo '1. Perform system update & upgrade?'
+echo '2. Install favourite applications?'
+echo '3. Install favourite system utilities?'
+echo '4. Install development tools?'
+echo '5. Install design tools?'
+echo '6. Install extra GNOME components?'
+echo '7. Install Ubuntu Restricted Extras?'
+echo '8. Install third-party applications?'
+echo '9. Customize system?'
+echo '10. Cleanup the system?'
+echo 'q. Quit?'
+echo ''
+read -p 'What would you like to do? (Enter your choice) : ' REPLY
+case $REPLY in
+    1) sysupgrade;; # System Upgrade
+    2) clear && favourites;; # Install Favourite Applications
+    3) clear && system;; # Install Favourite Tools
+    4) clear && development;; # Install Dev Tools
+    5) clear && design;; # Install Design Tools
+    6) clear && gnome;; # Install GNOME components
+    7) clear && codecs;; # Install Ubuntu Restricted Extras
+    8) clear && thirdparty;; # Install Third-Party Applications
+    9) clear && customize;; # Customize system
+    10) clear && cleanup;; # Cleanup System
+    [Qq]* ) echo '' && quit;; # Quit
+    * ) clear && echo 'Not an option, try again.' && main;;
+esac
+}
+
+#----- RUN MAIN FUNCTION -----#
+main
+
+#END OF SCRIPT
