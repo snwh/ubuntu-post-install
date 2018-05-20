6cfa800ab50ff4123d81a612766d021bd6ba5e7f
Sam Hewitt <sam@snwh.org>
2018-05-20 15:05:05 -0400

    massive overhaul for 18.04
     - rewrite includes flatpak and snap installs
     - even more generic/modular functions
     - fixes and efficiencies
     - deletion of unused app functions

--- ubuntu-post-install-script.sh
+++ ubuntu-post-install-script.sh
@@ -5,15 +5,15 @@
 #   Sam Hewitt <sam@snwh.org>
 #
 # Description:
-#   A set of post-installation script for Ubuntu and Ubuntu-based distributions
+#   A post-installation bash script for Ubuntu
 #
 # Legal Preamble:
 #
-# This project is free software; you can redistribute it and/or modify it under
+# This script is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
 # Foundation; version 3.
 #
-# This project is distributed in the hope that it will be useful, but WITHOUT
+# This script is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 # details.
@@ -25,105 +25,110 @@
 tabs 4
 clear
 
-# Import functions
-
-dir=$(dirname "$0")
-
-. $dir/functions/check
-. $dir/functions/cleanup
-. $dir/functions/codecs
-. $dir/functions/configure
-. $dir/functions/development
-. $dir/functions/favs
-. $dir/functions/node_apps
-. $dir/functions/fonts
-. $dir/functions/gnome
-. $dir/functions/password
-. $dir/functions/thirdparty
-. $dir/functions/update
-. $dir/functions/utilities
-
-
-# Fancy colorful echo messages
-function echo_message(){
-	local color=$1;
-	local exp=$2;
-	if ! [[ $color =~ '^[0-9]$' ]] ; then
-		case $(echo -e $color | tr '[:upper:]' '[:lower:]') in
-			# 0 = black
-			title) color=0 ;;
-			# 1 = red
-			error) color=1 ;;
-			# 2 = green
-			info) color=2 ;;
-			# 3 = yellow
-			warning) color=3 ;;
-			# 4 = blue
-			question) color=4 ;;
-			# 5 = magenta
-			success) color=5 ;;
-			# 6 = cyan
-			header) color=6 ;;
-			# 7 = white
-			*) color=7 ;;
-		esac
-	fi
-	tput bold;
-	tput setaf $color;
-	echo $exp;
-	tput sgr0;
-}
+# Title of script set
+TITLE="Ubuntu Post-Install Script"
 
 # Main
 function main {
-	echo_message title "Starting 'main' function..."
+	echo_message header "Starting 'main' function"
 	# Draw window
 	MAIN=$(eval `resize` && whiptail \
 		--notags \
-		--title "Ubuntu Post-Install Script" \
+		--title "$TITLE" \
 		--menu "\nWhat would you like to do?" \
 		--cancel-button "Quit" \
 		$LINES $COLUMNS $(( $LINES - 12 )) \
-		update		'Perform system update' \
-		favs		'Install preferred applications' \
-		utilities	'Install preferred CLI utilities' \
-		development	'Install preferred development tools' \
-		codecs		'Install non-free codecs' \
-		fonts		'Install additional fonts' \
-		thirdparty	'Install third-party applications' \
-		gnome		'Install latest GNOME software' \
-		node_apps	'Install NodeJS-based tools' \
-		configure	'Configure system' \
-		cleanup		'Cleanup the system' \
+		'system_update'			'Perform system update' \
+		'install_favs'			'Install preferred applications' \
+		'install_favs_utils'	'Install preferred utilities' \
+		'install_favs_dev'		'Install preferred development tools' \
+		'install_gnome'			'Install preferred GNOME software' \
+		'install_codecs'		'Install multimedia codecs' \
+		'install_fonts'			'Install additional fonts' \
+		'install_snap_apps'		'Install Snap applications' \
+		'install_flatpak_apps'	'Install Flatpak applications' \
+		'install_thirdparty'	'Install third-party applications' \
+		'system_configure'		'Configure system' \
+		'system_cleanup'		'Cleanup the system' \
 		3>&1 1>&2 2>&3)
-
 	# check exit status
-	EXITSTATUS=$?
-	if [ $EXITSTATUS = 0 ]; then
+	if [ $? = 0 ]; then
+		echo_message header "Starting '$MAIN' function"
 		$MAIN
 	else
+		# Quit
 		quit
 	fi
 }
 
+
 # Quit
 function quit {
-	echo_message title "Starting 'quit' function..."
-	if (whiptail --title "Quit" --yesno "Are you sure you want quit?" 8 64) then
-		echo "Exiting..."
-		echo_message success 'Thanks for using!'
+	echo_message header "Starting 'quit' function"
+	echo_message title "Exiting $TITLE..."
+	# Draw window
+	if (whiptail --title "Quit" --yesno "Are you sure you want quit?" 10 60) then
+		echo_message welcome 'Thanks for using!'
 		exit 99
 	else
 		main
 	fi
 }
 
-# Welcome message
-echo_message header "Ubuntu Post-Install Script"
-# Run check
-check
+# Import Functions
+function import_functions {
+	DIR="functions"
+	# iterate through the files in the 'functions' folder
+	for FUNCTION in $(dirname "$0")/$DIR/*; do
+		if [[ -d $FUNCTION ]]; then
+			continue
+		elif [[ -f $FUNCTION ]]; then
+			# source the function file
+			. $FUNCTION
+		fi
+	done
+}
 
-# Main
+# Fancy colorful echo messages
+function echo_message(){
+	local color=$1;
+	local message=$2;
+	if ! [[ $color =~ '^[0-9]$' ]] ; then
+		case $(echo -e $color | tr '[:upper:]' '[:lower:]') in
+			# black
+			header) color=0 ;;
+			# red
+			error) color=1 ;;
+			# green
+			success) color=2 ;;
+			# yellow
+			welcome) color=3 ;;
+			# blue
+			title) color=4 ;;
+			# purple
+			info) color=5 ;;
+			# cyan
+			question) color=6 ;;
+			# orange
+			warning) color=202 ;;
+			# white
+			*) color=7 ;;
+		esac
+	fi
+	tput bold;
+	tput setaf $color;
+	echo '-- '$message;
+	tput sgr0;
+}
+
+
+# Welcome message
+echo_message welcome "$TITLE"
+# Import main functions
+import_functions
+# Run system checks
+system_checks
+# main
 while :
 do
 	main
