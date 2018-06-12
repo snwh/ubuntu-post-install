edf8de0a9cea8df6bc98f65a872db2cd2db63d17
Sam Hewitt <sam@snwh.org>
2018-06-12 10:02:08 -0400

    fix where readme was being sourced

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -79,7 +79,11 @@ function import_functions {
 	DIR="functions"
 	# iterate through the files in the 'functions' folder
 	for FUNCTION in $(dirname "$0")/$DIR/*; do
+		# skip directories
 		if [[ -d $FUNCTION ]]; then
+			continue
+		# exclude markdown readmes
+		elif [[ $FUNCTION == *.md ]]; then
 			continue
 		elif [[ -f $FUNCTION ]]; then
 			# source the function file
